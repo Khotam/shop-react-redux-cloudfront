390d41d9ca8d5f08682f515e896f9273fcfe8556
Khotam Bakhromov <khotam_bakhromov@epam.com>
2021-08-29 23:54:08 +0500

    task 3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
-
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://oam6u551r2.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://oam6u551r2.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://oam6u551r2.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://oam6u551r2.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://oam6u551r2.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
