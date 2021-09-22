74d65c6a82156c94bd2b8d0a81a28ea2851c1f7b
Khotam <bakhromov17@gmail.com>
2021-09-22 22:21:08 +0500

    task-5 completed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://qdlj2uel14.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
 };
