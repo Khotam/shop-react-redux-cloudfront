b970656b820019cdb4d2a91ed0a175b66b853678
Khotam <bakhromov17@gmail.com>
2021-11-01 01:34:28 +0500

    complete: task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,6 @@ const API_PATHS = {
   order: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://qdlj2uel14.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://e8phho4pci.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://khotam-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 export default API_PATHS;
